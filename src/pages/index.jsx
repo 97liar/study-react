26bb92a87d9ef4dab91e5372e59376ed6cde62e7
97liar <97liar@gmail.com>
2021-08-12 02:54:40 +0900

    ✨ 講座用のuserEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 export default function Home() {
 
@@ -15,6 +15,16 @@ const handleClick = useCallback((e) => {
   e.preventDefault();
   alert(foo);
 },[]);
+
+useEffect(() => {
+  console.log("マウント");
+  document.body.style.backgroundColor = "lightblue";
+
+  return () => {
+  console.log("アンマウント");
+  document.body.style.backgroundColor = "";
+  }
+},[])
 
   return (
     <div className={styles.container}>
